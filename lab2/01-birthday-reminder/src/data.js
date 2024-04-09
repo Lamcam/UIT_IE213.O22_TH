0187f30f0d3b914aaf0d0ca726c066e4cda4ca9f
anhuynh219 <anhuynh200319@gmail.com>
2024-04-09 21:34:43 +0700

    Submit lab2

---
+++ lab2/01-birthday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Huỳnh An Nghiệp',
+    age: 20,
+    image:
+      'https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-1/431619367_3273721916265552_366076219677146532_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=aVU8m0clY6kAb4DbAKk&_nc_ht=scontent.fhan3-3.fna&oh=00_AfBG43F7An9HQ3ljZzz7HjezncgIDbhI-sFOSDnhgop2SQ&oe=6615DBE2',
+  },
+  {
+    id: 2,
+    name: 'Bùi Yến Giàu',
+    age: 20,
+    image:
+      'https://scontent.fhan4-5.fna.fbcdn.net/v/t39.30808-1/432998733_2679660185520914_1567289916463564546_n.jpg?stp=dst-jpg_p200x200&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=NFWNiGc5O_gAb4qr7RX&_nc_ht=scontent.fhan4-5.fna&oh=00_AfACyPCGeSmXSuhBNrUCf8n_z6hdu0-blOq8Kbj1AUfrtQ&oe=6615D964',
+  },
+  {
+    id: 3,
+    name: 'Bùi Xuân Nhi',
+    age: 20,
+    image:
+      'https://scontent.fhan4-5.fna.fbcdn.net/v/t39.30808-1/435250692_2205030023170570_6069152540062607589_n.jpg?stp=dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=T7NvVQcX0ZUAb49gFeP&_nc_ht=scontent.fhan4-5.fna&oh=00_AfA0nJ9zrvLtqKJ3DnGk8_kPmPnCJcD309vNvHEUZpew3A&oe=6615E262',
+  },
+  {
+    id: 4,
+    name: 'Trịnh Hoài Nam',
+    age: 20,
+    image:
+      'https://scontent.fhan4-3.fna.fbcdn.net/v/t39.30808-1/433446274_1859485607797515_6300832813695397191_n.jpg?stp=dst-jpg_s200x200&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=2nDWgZKqRaQAb4Xlvbt&_nc_ht=scontent.fhan4-3.fna&oh=00_AfDLKndJtpkPxqwi2xJCKV3VdQg-bSItKcIi4-50np693A&oe=6615CB8A',
+  },
+  {
+    id: 5,
+    name: 'Lâm Thị Hồng Cẩm',
+    age: 20,
+    image:
+      'https://scontent.fhan4-3.fna.fbcdn.net/v/t39.30808-6/316093334_1560388234373922_4903038571052367714_n.jpg?stp=dst-jpg_s600x600&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Z4HXKU3GbLgAb71tATW&_nc_ht=scontent.fhan4-3.fna&oh=00_AfAEn2M3DupsdLY1_UbM_hMKBPV_A-Av_X0PavMVf3GNGg&oe=6615CC88',
+  },
+];
